80b34fe3c66bcde0a2e1835b32fe9786f7688227
arbreshashabanaj <125255290+arbreshashabanaj@users.noreply.github.com>
2023-05-12 01:40:52 +0200

    Add files via upload

--- mini upggift jc/js/script.js
+++ mini upggift jc/js/script.js
@@ -1,90 +1,86 @@
-document.addEventListener("DOMContentLoaded", function()  {
-    for (let i = 1; i < 6; i++) {
-        const h2Element = document.createElement('h2');
-        document.body.appendChild(h2Element);
-        h2Element.innerText = "rad" + i;
-   
-       h2Element.style.backgroundColor = `hsl(${90 + i * 20}, 70%, 70%)`;
-       console.log(90 + i * 20);
-       h2Element.style.fontSize = i * 5 + "px";
-       h2Element.style.textAlign = "center";
+
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const headerColors = ["hsl(110, 70%, 70%)", "hsl(130, 70%, 70%)", "hsl(150, 70%, 70%)", "hsl(170, 70%, 70%)", "hsl(190, 70%, 70%)"];
+    const numbers = ["ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio", "tio"];
+  
+    for (let i = 0; i < 5; i++) {
+      const h2Element = document.createElement('h2');
+      h2Element.innerText = "rad" + (i + 1);
+      h2Element.style.backgroundColor = headerColors[i];
+      h2Element.style.fontSize = (i + 1) * 5 + "px";
+      h2Element.style.textAlign = "center";
+      document.body.appendChild(h2Element);
     }
-   
-   
+  
     const container = document.createElement('div');
-    document.body.append(container);
-   
     container.style.border = '1px solid black';
     container.style.padding = '5rem';
     container.style.display = 'flex';
     container.style.justifyContent = 'space-around';
-   
-    for (let i = 0; i <3; i++) {
-       const smallContainer = document.createElement('div');
-       container.append(smallContainer);
-       smallContainer.style.backgroundColor = "violet";
-       smallContainer.style.padding = '1rem';
-       smallContainer.style.width ='5rem';
+  
+    for (let i = 0; i < 3; i++) {
+      const smallContainer = document.createElement('div');
+      smallContainer.style.backgroundColor = "violet";
+      smallContainer.style.padding = '1rem';
+      smallContainer.style.width = '5rem';
+  
+      if (i == 0) {
+        for (let j = 0; j < 10; j++) {
+          const h1 = document.createElement('h1');
+          h1.innerText = j;
+          h1.style.backgroundColor = j % 2 === 0 ? "black" : "white";
+          h1.style.color = j % 2 === 0 ? "white" : "black";
+          h1.style.margin = '0px';
+  
+          if (j === 4) {
+            h1.style.backgroundColor = "";
+          }
+  
+          smallContainer.appendChild(h1);
+        }
+      } else if (i === 1) {
+        for (let j = 9; j >= 0; j--) {
+          const h1 = document.createElement('h1');
+          h1.innerText = j;
+          h1.style.textAlign = 'center';
+          h1.style.backgroundColor = j % 2 === 0 ? "black" : "white";
+          h1.style.color = j % 2 === 0 ? "white" : "black";
+          h1.style.margin = '0px';
+  
+          if (j === 8) {
+            h1.style.backgroundColor = "";
+          }
+  
+          smallContainer.appendChild(h1);
+        }
+      } else {
+        for (let j = 0; j < 10; j++) {
+          const h1 = document.createElement('h1');
+          h1.innerText = numbers[j];
+          h1.style.textAlign = 'right';
+          h1.style.backgroundColor = j % 2 === 0 ? "black" : "white";
+          h1.style.color = j % 2 === 0 ? "white" : "black";
+          h1.style.margin = '0px';
+          smallContainer.appendChild(h1);
+
+          if (j === 5) {
+            h1.style.backgroundColor = "";
+          }
+  
+        }
+      }
+  
+      container.appendChild(smallContainer);
     }
-   
-    if (i == 0){
-       for(let i = 0; i <10; i++){
-           const h1 = document.createElement('h1');
-           smallContainer.append(h1);
-   
-           h1.innerText = i;
-           
-   
-           h1.style.backgroundColor = `HSL(0, 0%, 100%`;
-           h1.style.margin = '0px';
-           if(i%2==0){
-               h1.style.backgroundColor = `HSL(0, 0%, 0%)`;
-               h1.style.color = `HSL(0, 0%, 100%`;
-           }
-           if (i == 4){
-               h1.style.backgroundColor = null;
-           }
-       }
-   
-    } else if(i == 1){
-       for (let i = 9; i >=0; i--) {
-           const h1 = document.createElement('h1');
-           smallContainer,append(h1);
-           h1.style.textAlign = 'center';
-           h1.style.backgroundColor = `HSL(0, 0%, 100%`;
-   
-           h1.innerText = i;
-           h1.style.margin = '0px';
-           if(i%2==0){
-               h1.style.backgroundColor = `HSL(0, 0%, 0%)`;
-               h1.style.color = `HSL(0, 0%, 100% `;
-   
-           }
-           if(i == 8){
-   
-           h1.style.backgroundColor = null;
-   
-           }
-       }
-   
-   }else {
-       const numbers = [ "ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio", "tio"];
-       for (let i = 0; i <10; i++){
-           const h1 = document.createElement('h1');
-           smallContainer.append(h1);
-           h1.style.textAlign = 'right';
-           h1.style.backgroundColor = `HSL(0, 0%, 100%`;
-   
-           h1.innerText =numbers[i];
-           h1.style.margin = '0px';
-           if (i%2==0){
-               h1.style.backgroundColor = `HSL(0, 0%, 0%)`;
-               h1.style.color = `HSL(0, 0%, 100%`;
-   
-           }if (i == 5) {
-               h1.style.backgroundColor = null;     
-           }    
-       }
-   }
-   });
-   
+  
+    document.body.appendChild(container);
+  });
+  
+  
+
+
+
+
+
